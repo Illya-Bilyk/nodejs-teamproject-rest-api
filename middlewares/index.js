221520bcc432103f08ad0a6823fdc53f48cd9cef
Illya-Bilyk <bilikillya.i@gmail.com>
2023-11-02 21:06:49 +0200

    bug fix in updateUser

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,11 +1,9 @@
 const validateBody = require("./validateBody");
 const authenticate = require("./authenticate");
 const isValidId = require("./isValidId");
-const isValidUserId = require("./isValidUserId");
 
 module.exports = {
   validateBody,
   authenticate,
   isValidId,
-  isValidUserId,
 };
