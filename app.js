2c8cc35c8a72be9aaaccfc61fba680f95b2b06ce
go-to-stars <ivanstep@ukr.net>
2023-11-08 14:28:57 +0200

    added corrections

--- app.js
+++ app.js
@@ -19,8 +19,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api-docs", swaggerUi.serve);
-app.get("/api-docs", swaggerUi.setup(swaggerDocument));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/auth", authRouter);
 app.use("/users", usersRouter);
